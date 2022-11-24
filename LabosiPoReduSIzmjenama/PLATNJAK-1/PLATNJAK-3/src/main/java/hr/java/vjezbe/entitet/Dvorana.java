c7a4496dbbad6dde2fb116f04cef90f66cf851be
Nikola Platnjak <102035430+PlatnjakNikola@users.noreply.github.com>
2022-11-24 13:20:42 +0100

    Delete LabosiPoReduSIzmjenama/PLATNJAK-1 directory

--- LabosiPoReduSIzmjenama/PLATNJAK-1/PLATNJAK-3/src/main/java/hr/java/vjezbe/entitet/Dvorana.java
+++
@@ -1,8 +0,0 @@
-package hr.java.vjezbe.entitet;
-
-/**
- * record for dvorana
- * @param naziv is name of dvorana
- * @param zgrada is building where dvorana is
- */
-public record Dvorana(String naziv, String zgrada) {}
